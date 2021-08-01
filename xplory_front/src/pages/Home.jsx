4310d433b559093bb72d0763566b9a252d48306c
brissybee <techiemum@workmail.com>
2021-08-01 17:14:07 +1000

    Update Home.jsx

--- xplory_front/src/pages/Home.jsx
+++ xplory_front/src/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import { Button } from "@material-ui/core";
 
-export default function Home() {
+export default function Home(props) {
 	return (
 		<div>
 			<h1>Home Page</h1>
 
-			<Button variant='contained' color='primary'>
+			<Button onClick={()=>props.history.push('login')} variant='contained' color='primary'>
 				Hello Team
 			</Button>
 		</div>
