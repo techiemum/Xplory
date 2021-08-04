72457aac94e2793b6e9c3dc176c049f866223af4
brissybee <techiemum@workmail.com>
2021-08-04 16:57:32 +1000

    Update Home.js

--- xplory_front/src/components/Home.js
+++ xplory_front/src/components/Home.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-import { Button } from "@material-ui/core";
+import { Button } from "reactstrap";
+
+import Maps from './Maps'
 
 export default function Home(props) {
 	return (
 		<div>
 			<h1>Home Page</h1>
 
-			<Button onClick={()=>props.history.push('login')} variant='contained' color='primary'>
+			<Button onClick={()=>props.history.push('login')} color='primary'>
 				Hello Team
 			</Button>
+			<Maps/>
 		</div>
 	);
 }
-import React from "react";
-
-import { Button } from "@material-ui/core";
 
