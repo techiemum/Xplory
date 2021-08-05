cecff7a82e96a58af8a8a1c51bcd355208605397
brissybee <techiemum@workmail.com>
2021-08-05 12:44:24 +1000

    Update Home.js

--- xplory_front/src/components/Home.js
+++ xplory_front/src/components/Home.js
@@ -1,19 +1,23 @@
 import React from "react";
-
+import Home from './Home';
+import Filter from './Filter';
+import About from './About';
+import LoginForm from './LoginForm';
 import { Button } from "reactstrap";
-
 import Maps from './Maps'
 
 export default function Home(props) {
 	return (
-		<div>
-			<h1>Home Page</h1>
-
-			<Button onClick={()=>props.history.push('login')} color='primary'>
-				Hello Team
-			</Button>
+	
+		// 	{/* <Button onClick={()=>props.history.push('login')} color='primary'>
+		// 		Hello Team
+		// 	</Button> */}
+	// <Home/>
+	// <Filter/>
+	// <About/>
+	// <LoginForm/>
 			<Maps/>
-		</div>
+		
 	);
 }
 
