03308e740f6061351ad4b6f8333988043162cbd1
brissybee <techiemum@workmail.com>
2021-08-05 12:44:27 +1000

    Create Navigation.js

---
+++ xplory_front/src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Home from './Home';
+import Filter from './Filter';
+import About from './About';
+import LoginForm from './LoginForm';
+
+const Navigation =()=>{
+	return(
+		
+	<>
+	<Home/>
+	<Filter/>
+	<About/>
+	<LoginForm/>
+	
+	
+	</>	
+	)
+}
+
+export default Navigation
